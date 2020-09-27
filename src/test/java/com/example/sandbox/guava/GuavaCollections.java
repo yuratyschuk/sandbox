4502e472c24484a6ab4adab633b9fad69940c6f9
Tyshchuk <YTyshchuk@luxoft.com>
2020-09-27 13:11:19 +0300

    Refactored GuavaCollections Tests. Added asserts

--- src/test/java/com/example/sandbox/guava/GuavaCollections.java
+++ src/test/java/com/example/sandbox/guava/GuavaCollections.java
@@ -2,13 +2,15 @@ package com.example.sandbox.guava;
 
 import com.example.sandbox.model.Person;
 import com.google.common.collect.*;
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
+import org.testng.internal.collections.Ints;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Log4j2
 public class GuavaCollections {
 
     private final Person person1 = new Person(1, "John", "Ivanov", 20, new Date());
@@ -19,7 +21,7 @@ public class GuavaCollections {
 
 
     @Test
-    public void multiSet() {
+    public void testHashMultiset() {
         Multiset<String> stringMultiset = HashMultiset.create();
         stringMultiset.add("test");
         stringMultiset.add("test");
@@ -34,151 +36,159 @@ public class GuavaCollections {
         assertThat(stringMultiset.count("test")).isEqualTo(1);
 
         stringMultiset.setCount("test", 25);
+
         assertThat(stringMultiset.count("test")).isEqualTo(25);
 
+    }
+
+    @Test
+    public void testLinkedHashMultiset() {
         Multiset<String> stringLinkedMultiSet = LinkedHashMultiset.create();
         stringLinkedMultiSet.add("test");
         stringLinkedMultiSet.add("test1");
         stringLinkedMultiSet.add("test2");
         stringLinkedMultiSet.add("test2");
         stringLinkedMultiSet.add("test");
-        System.out.println(stringMultiset.entrySet());
-        System.out.println(stringLinkedMultiSet.entrySet());
 
+        assertThat(stringLinkedMultiSet).contains("test2", "test", "test1");
+        assertThat(stringLinkedMultiSet.count("test2")).isEqualTo(2);
+        assertThat(stringLinkedMultiSet.elementSet()).containsSequence("test", "test1", "test2");
+        assertThat(stringLinkedMultiSet.size()).isEqualTo(5);
+
+    }
+
+    @Test
+    public void testTreeMultiset() {
         Multiset<Person> personMultiset = TreeMultiset.create();
         personMultiset.add(person1);
         personMultiset.add(person2);
         personMultiset.add(person3);
-        System.out.println(personMultiset.entrySet());
+
+        assertThat(personMultiset).containsSequence(person3, person1, person2);
     }
 
     @Test
-    public void multiMap() {
+    public void testArrayListMultimap() {
         Multimap<String, Person> personArrayMultiMap = ArrayListMultimap.create();
 
-        personArrayMultiMap.put("first person", person1);
-        personArrayMultiMap.put("second person", person2);
-        personArrayMultiMap.put("third person", person3);
+        personArrayMultiMap.put("first", person1);
+        personArrayMultiMap.put("second", person2);
+        personArrayMultiMap.put("third", person3);
 
-        System.out.println(personArrayMultiMap.keySet());
-        System.out.println(personArrayMultiMap.entries());
-        System.out.println(personArrayMultiMap.asMap().get("first person"));
+        assertThat(personArrayMultiMap.get("first")).containsExactly(person1);
 
+
+    }
+
+    @Test
+    public void testPersonHashMultimap() {
         Multimap<String, Person> personHashMultimap = HashMultimap.create();
-        personHashMultimap.put("first person", person1);
-        personHashMultimap.put("second person", person2);
-        personHashMultimap.put("third person", person3);
+        personHashMultimap.put("first", person1);
+        personHashMultimap.put("second", person2);
+        personHashMultimap.put("third", person3);
 
-        System.out.println(personHashMultimap.keySet());
-        System.out.println(personHashMultimap.entries());
-        System.out.println(personHashMultimap.asMap().keySet());
-        System.out.println(personHashMultimap.asMap().entrySet());
+        assertThat(personHashMultimap.keySet()).containsSequence("second", "third", "first");
 
+    }
+
+    @Test
+    public void testLinkedListMultimap() {
         Multimap<String, Person> personLinkedList = LinkedListMultimap.create();
         personLinkedList.put("first person", person1);
         personLinkedList.put("second person", person2);
         personLinkedList.put("third person", person3);
-        personLinkedList.put("third person", person1);
 
-        System.out.println(personLinkedList.entries());
+        assertThat(personLinkedList.values()).containsSequence(person1, person2, person3);
 
-        Multimap<String, Person> linkedHashMultiMap = LinkedHashMultimap.create();
+    }
 
-        linkedHashMultiMap.put("first person", person1);
-        linkedHashMultiMap.put("second person", person2);
-        linkedHashMultiMap.put("third person", person3);
-        linkedHashMultiMap.put("third person", person1);
+    @Test
+    public void testHashBimap() {
+        HashBiMap<String, Person> hashBimap = HashBiMap.create();
+        hashBimap.put("first", person1);
+        hashBimap.put("second", person2);
+        hashBimap.put("third", person3);
 
-        System.out.println(linkedHashMultiMap.entries());
+        assertThat(hashBimap.inverse().get(person1)).isEqualTo("first");
+        assertThat(hashBimap.get("first")).isEqualByComparingTo(person1);
+
+    }
+
+    @Test
+    public void testGuavaTreeBasedTable() {
+        Table<Integer, Person, String> treeBasedTable = TreeBasedTable.create();
+        treeBasedTable.put(1, person1, "first");
+        treeBasedTable.put(2, person2, "second");
+        treeBasedTable.put(3, person3, "third");
+
+        assertThat(treeBasedTable.row(1)).containsEntry(person1, "first");
+        assertThat(treeBasedTable.column(person1)).containsEntry(1, "first");
 
 
     }
 
     @Test
-    public void biMap() {
-        HashBiMap<String, Person> hashBiMap = HashBiMap.create();
+    public void testGuavaHashBasedTable() {
+        Table<Integer, Person, String> hashBasedTable = HashBasedTable.create();
+        hashBasedTable.put(1, person1, "first");
+        hashBasedTable.put(2, person2, "second");
+        hashBasedTable.put(3, person3, "third");
 
-        hashBiMap.put("first person", person1);
-        hashBiMap.put("second person", person2);
-        hashBiMap.put("third person", person3);
-
-        String test = hashBiMap.inverse().get(person1);
-
-        Person personFind = hashBiMap.get("first person");
-        System.out.println(test + " " + personFind);
-
-
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put(2, "second");
+        assertThat(hashBasedTable.columnMap()).containsEntry(person2, linkedHashMap);
     }
 
     @Test
-    public void testGuavaTable() {
-        Table<Person, String, String> treeBasedTable = TreeBasedTable.create();
-        treeBasedTable.put(person1, "person 1", "1");
-        treeBasedTable.put(person2, "person 2", "2");
-        treeBasedTable.put(person3, "person 3", "3");
-
-        System.out.println(treeBasedTable.rowKeySet());
-        System.out.println(treeBasedTable.cellSet());
-        System.out.println(treeBasedTable.columnKeySet());
-        System.out.println(treeBasedTable.columnMap());
-        System.out.println(treeBasedTable.rowMap());
-
-        Table<Person, String, String> hashBasedTable = HashBasedTable.create();
-        hashBasedTable.put(person1, "person 1", "1");
-        hashBasedTable.put(person2, "person 2", "2");
-        hashBasedTable.put(person3, "person 3", "3");
-
-        System.out.println(hashBasedTable.columnMap());
-
-
-    }
-
-    @Test
-    public void testClassToInstanceMap() {
+    public void testMutableClassToInstanceMap() {
         ClassToInstanceMap<Object> objectMap = MutableClassToInstanceMap.create();
 
         objectMap.putInstance(Integer.class, 1);
         objectMap.putInstance(Person.class, person1);
 
-        System.out.println(objectMap.get(Person.class));
-
+        assertThat(objectMap.getInstance(Integer.class)).isEqualTo(1);
+        assertThat(objectMap.getInstance(Person.class)).isEqualTo(person1);
+        assertThat(objectMap).containsEntry(Person.class, person1);
 
     }
 
     @Test
-    public void testRangeSet() {
+    public void testTreeRangeSet() {
         RangeSet<Integer> rangeSet = TreeRangeSet.create();
-        rangeSet.add(Range.closed(1, 10)); // {[1, 10]}
-        rangeSet.add(Range.closedOpen(11, 15)); // disconnected range: {[1, 10], [11, 15)}
-        rangeSet.add(Range.closedOpen(15, 20)); // connected range; {[1, 10], [11, 20)}
-        rangeSet.add(Range.openClosed(0, 0)); // empty range; {[1, 10], [11, 20)}
-        rangeSet.remove(Range.open(5, 10)); // splits [1, 10]; {[1, 5], [10, 10], [11, 20)}
+        rangeSet.add(Range.closed(1, 10));
+        rangeSet.add(Range.closedOpen(11, 15));
+        rangeSet.add(Range.closedOpen(15, 20));
 
-        System.out.println(rangeSet.asDescendingSetOfRanges());
+        assertThat(rangeSet.asRanges()).hasSize(2);
+        assertThat(rangeSet.contains(5)).isTrue();
     }
 
     @Test
-    public void testRangeMap() {
+    public void testTreeRangeMap() {
         RangeMap<Integer, String> rangeMap = TreeRangeMap.create();
-        rangeMap.put(Range.closed(1, 10), "foo"); // {[1, 10] => "foo"}
-        rangeMap.put(Range.open(3, 6), "bar"); // {[1, 3] => "foo", (3, 6) => "bar", [6, 10] => "foo"}
-        rangeMap.put(Range.open(10, 20), "foo"); // {[1, 3] => "foo", (3, 6) => "bar", [6, 10] => "foo", (10, 20) => "foo"}
-        rangeMap.remove(Range.closed(5, 11)); // {[1, 3] => "foo", (3, 5) => "bar", (11, 20) => "foo"}
+        rangeMap.put(Range.closed(1, 10), "foo");
+        rangeMap.put(Range.open(3, 6), "bar");
+        rangeMap.put(Range.open(10, 20), "foo");
 
-        System.out.println(rangeMap.asMapOfRanges());
+        assertThat(rangeMap.get(11)).isNotNull();
+        assertThat(rangeMap.span())
+                .accepts(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+                .rejects(20);
     }
 
     @Test
     public void testImmutableCollection() {
-        ImmutableList<Person> immutableList = ImmutableList.<Person>builder().add(person1, person2, person3).build();
-        System.out.println(immutableList);
+        ImmutableList<Person> immutableList = ImmutableList
+                .<Person>builder()
+                .add(person1, person2, person3)
+                .build();
 
         List<Person> personList = immutableList.asList();
+        assertThat(personList).isEqualTo(immutableList);
 
-        System.out.println(personList);
         personList = ImmutableList.copyOf(immutableList);
+        assertThat(personList).isEqualTo(immutableList);
 
-        System.out.println(personList);
     }
 
     @Test
@@ -188,12 +198,27 @@ public class GuavaCollections {
         System.out.println(Collections2.filter(personList, person -> person.getAge() == 16 &&
                 person.getFirstName().equals("Roman")));
 
-        System.out.println(Collections2.transform(personList, person -> person.getFirstName() + "test"));
+        Collection<Person> filterPerson = Collections2.filter(personList, person -> person.getAge() == 16 &&
+                person.getFirstName().equals("Roman"));
 
-        System.out.println(Collections2.orderedPermutations(personList, Ordering.natural()));
+        assertThat(filterPerson.size()).isEqualTo(1);
 
+        Collection<String> transformPerson = Collections2
+                .transform(personList, person -> person.getFirstName() + "test");
+
+        assertThat(transformPerson).containsExactly("Johntest", "Romantest", "Jamestest");
 
     }
 
+    @Test
+    public void testIterables() {
+        List<Person> personList = Lists.newArrayList(person1, person2, person3);
 
+        Iterable<Integer> concatenated = Iterables.concat(
+                Ints.asList(1, 2, 3),
+                Ints.asList(4, 5, 6));
+
+        assertThat(concatenated.iterator().next()).isEqualTo(1);
+        assertThat(Iterables.getLast(personList)).isEqualByComparingTo(person3);
+    }
 }
