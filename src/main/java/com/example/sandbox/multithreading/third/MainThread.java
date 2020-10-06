fd1dca30375e1fed2eb2eaa04d7f60dc44a64e52
Tyshchuk <YTyshchuk@luxoft.com>
2020-10-06 13:35:39 +0300

    Added countDownLatch to Deadlock task

--- src/main/java/com/example/sandbox/multithreading/third/MainThread.java
+++ src/main/java/com/example/sandbox/multithreading/third/MainThread.java
@@ -4,32 +4,34 @@ import java.util.concurrent.CountDownLatch;
 
 public class MainThread {
 
-//    private static final CountDownLatch countDownLatch = new CountDownLatch(2);
-    public static void main(String[] args)  {
+    private static final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+    public static void main(String[] args) {
         String recourse1 = "first";
 
         String recourse2 = "second";
 
 
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             Thread thread1 = new Thread(() -> {
-//                countDownLatch.countDown();
-//                try {
-//                    countDownLatch.await();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
 
                 synchronized (recourse1) {
-                    System.out.println("Thread1. Deadlock with recourse1: " + recourse1);
+                    try {
+                        countDownLatch.await();
+                        System.out.println("Thread1. Deadlock with recourse1: " + recourse1);
 
-                    synchronized (recourse2) {
-                        System.out.println("Thread1. Recourse2: " + recourse2);
+                        synchronized (recourse2) {
+                            System.out.println("Thread1. Recourse2: " + recourse2);
+                        }
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             });
 
             Thread thread2 = new Thread(() -> {
+                countDownLatch.countDown();
                 synchronized (recourse2) {
                     System.out.println("Thread2. Deadlock with recourse2: " + recourse2);
 
